715de275f3694bcddcfc32522d08e4683f8255a5
Fredy <156226299+FrederikRek@users.noreply.github.com>
2024-01-30 10:46:32 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,17 @@
+    const data = [];
+function generateRandomData() {
+
+        console.log("Random Number: " + item);
+}
+    data.forEach(item => {
+    const data = generateRandomData();
+    return data;
+
+    });
+main();
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+function main() {
+
+    }
